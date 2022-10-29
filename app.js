f3e982ed439463c26b6d7fb449da410599d7bb9f
ZukaLejava <lejavazuka@gmail.com>
2022-10-29 04:13:57 +0400

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+let hide=document.getElementById("hide")
+let more=document.getElementById("more")
+
+function func(){
+    document.getElementById("more").style.transform="translate(0px,-50px)"
+    more.style.transition="1.5s"
+}
+function func1(){
+    document.getElementById("more").style.transform="translate(-1500px)"
+    more.style.transition="1.5s"
+}
